a863ce23445de0c3eedecc2f30694b71bb22f374
simonbystrom <simonbystrom@Simons-MacBook-Air.local>
2020-10-19 13:05:51 +0900

    Updates

---
+++ precache-manifest.ee178742bf18ecfe42c1e7666bd17a40.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd8f458349a7b041fe3c1600b1510303",
+    "url": "/YouTube-clone-DESIGN-/index.html"
+  },
+  {
+    "revision": "aaf174df3b556a826cb2",
+    "url": "/YouTube-clone-DESIGN-/static/css/2.235ad548.chunk.css"
+  },
+  {
+    "revision": "ca013cc1bc80bc0d1583",
+    "url": "/YouTube-clone-DESIGN-/static/css/main.dc21a2db.chunk.css"
+  },
+  {
+    "revision": "aaf174df3b556a826cb2",
+    "url": "/YouTube-clone-DESIGN-/static/js/2.0c9eeeb4.chunk.js"
+  },
+  {
+    "revision": "656ea20e7001de4c6ac3ad4350aa0ffd",
+    "url": "/YouTube-clone-DESIGN-/static/js/2.0c9eeeb4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca013cc1bc80bc0d1583",
+    "url": "/YouTube-clone-DESIGN-/static/js/main.4a34beb5.chunk.js"
+  },
+  {
+    "revision": "771e299f15ca2c67967b",
+    "url": "/YouTube-clone-DESIGN-/static/js/runtime-main.cc6f2208.js"
+  },
+  {
+    "revision": "31d28485e1cf7369272270fd730327c0",
+    "url": "/YouTube-clone-DESIGN-/static/media/remixicon.31d28485.eot"
+  },
+  {
+    "revision": "881fbc46361e0c0e5f003c159b2f3005",
+    "url": "/YouTube-clone-DESIGN-/static/media/remixicon.881fbc46.woff"
+  },
+  {
+    "revision": "888e61f04316f10bddfff7bee10c6dd0",
+    "url": "/YouTube-clone-DESIGN-/static/media/remixicon.888e61f0.ttf"
+  },
+  {
+    "revision": "95138f36e015ad912c37db92164f5844",
+    "url": "/YouTube-clone-DESIGN-/static/media/remixicon.95138f36.svg"
+  },
+  {
+    "revision": "9915fef980fa539085da55b84dfde760",
+    "url": "/YouTube-clone-DESIGN-/static/media/remixicon.9915fef9.woff2"
+  }
+]);
